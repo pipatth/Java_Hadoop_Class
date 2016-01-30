5e638ac4c43f11a40a82155c62ba7ecfa378376c
pipat <pipat.th@gmail.com>
2016-01-30 15:59:59 -0600

    create classes

---
+++ src/cscie55/hw1/elevator/Elevator.java
@@ -0,0 +1,11 @@
+package cscie55.hw1.elevator;
+
+/**
+ * Created by pipat on 1/30/16.
+ */
+public class Elevator {
+    public static final int FLOORS = 7;
+
+
+
+}
---
+++ src/cscie55/hw1/elevatortest/ElevatorTest.java
@@ -0,0 +1,7 @@
+package cscie55.hw1.elevatortest;
+
+/**
+ * Created by pipat on 1/30/16.
+ */
+public class ElevatorTest {
+}

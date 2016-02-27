4fb0b43f94571df097180943cc20737dcb05a368
pipat <pipat.th@gmail.com>
2016-02-26 23:33:44 -0600

    commit change

---
+++ hw3/src/cscie55/hw3/Passenger.java
@@ -0,0 +1,47 @@
+package cscie55.hw3;
+
+public class Passenger {
+
+    /* FIELDS */
+
+
+
+
+    /* CONSTRUCTOR */
+
+
+
+    /* METHODS */
+
+    public int currentFloor()
+    {
+
+    }
+
+    public int destinationFloor()
+    {
+
+    }
+
+    public void waitForElevator(int newDestinationFloor)
+    {
+
+    }
+
+    public void boardElevator()
+    {
+
+    }
+
+    public void arrive()
+    {
+
+
+    }
+
+    public String toString()
+    {
+
+    }
+    
+}
